ab53300e98051db66677c2aae983240d4767e5b1
lgaid <104643045+lgaid@users.noreply.github.com>
2022-08-27 20:56:38 -0400

    Static Constants

--- src/java_ecommerce/StaticConstants.java
+++ src/java_ecommerce/StaticConstants.java
@@ -1,11 +1,14 @@
 package java_ecommerce;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class StaticConstants {
 
     public static final List<Customer> CUSTOMER_LIST = new ArrayList<>();
+
+
 
 
 
