9f3518d4a621dd28c1665a7608c726aeeb25bdbe
lgaid <104643045+lgaid@users.noreply.github.com>
2022-08-27 22:38:48 -0400

    Skincare

---
+++ src/category/Skincare.java
@@ -0,0 +1,21 @@
+package category;
+
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class Skincare extends Category{
+
+
+    public Skincare (UUID id,String name){
+        super(id,name);
+    }
+
+
+    @Override
+    public LocalDateTime findDeliveryDueDate() {
+        return LocalDateTime.now();
+    }
+
+
+}
