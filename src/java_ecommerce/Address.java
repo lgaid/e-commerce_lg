08eabea708904b8ca075715b76034d8f50fe72fb
lgaid <104643045+lgaid@users.noreply.github.com>
2022-08-27 20:39:30 -0400

    Address class

---
+++ src/java_ecommerce/Address.java
@@ -0,0 +1,24 @@
+package java_ecommerce;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class Address {
+
+    private String streetNumber;
+    private String streetName;
+    private String additionalAddressLine;
+    private String zipcode;
+    private String state;
+
+
+
+
+
+
+}
