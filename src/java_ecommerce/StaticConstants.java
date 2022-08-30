c33afc5c3ab51fd9666c8ba1c21bde5f7fb20e72
lgaid <104643045+lgaid@users.noreply.github.com>
2022-08-30 17:24:03 -0400

    Static Constants updated

--- src/java_ecommerce/StaticConstants.java
+++ src/java_ecommerce/StaticConstants.java
@@ -1,13 +1,27 @@
 package java_ecommerce;
 
 
+import balance.Balance;
+import category.Category;
+import discount.Discount;
+import order.Order;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class StaticConstants {
 
     public static final List<Customer> CUSTOMER_LIST = new ArrayList<>();
+    public static final List<Category> CATEGORY_LIST = new ArrayList<>();
+    public static final List<Product> PRODUCT_List= new ArrayList<>();
 
+    public static final List<Balance> Customer_Balance_List = new ArrayList<>();
+
+    public static final List<Balance> GiftCard_Balance_List = new ArrayList<>();
+
+    public static  final List<Discount> DISCOUNT_LIST = new ArrayList<>();
+
+    public static final List<Order> ORDER_LIST = new ArrayList<>();
 
 
 
