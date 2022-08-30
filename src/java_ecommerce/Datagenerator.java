d8774c51d098a056b026ac6bf72822d93d013353
lgaid <104643045+lgaid@users.noreply.github.com>
2022-08-30 16:52:18 -0400

    Data generator updated

--- src/java_ecommerce/Datagenerator.java
+++ src/java_ecommerce/Datagenerator.java
@@ -1,5 +1,16 @@
 package java_ecommerce;
 
+import balance.Balance;
+import balance.CustomerBalance;
+import balance.GiftCardBalance;
+import category.Category;
+import category.Electronic;
+import category.Furniture;
+import category.Skincare;
+import discount.AmountBasedDiscount;
+import discount.Discount;
+import discount.RateBasedDiscount;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -8,31 +19,90 @@ public class Datagenerator {
 
     public static void createCustomers(){
 
-        Address customer1address1 = new Address("14869","Gallows Rd","Suite 301","22042","VA");
-        Address customer1address2 = new Address("15230","Jermantown Drive","Apartment 1109", "20130", "VA");
-        Address customer2address1 = new Address("5895","Callow Terrace","A2","220152", "NJ");
+        Address address1Customer1 = new Address("7925","Jones Branch Dr","Suite 3300","22102","VA");
+        Address address2Customer1 = new Address("825","GeorgeTown Pky","Suite 5355","22036","VA");
+        Address address1Customer2 = new Address("5924","Lee Hwy","House","22044","VA");
 
         List<Address> customer1AddressList = new ArrayList<>();
 
-        customer1AddressList.add(customer1address1);
-        customer1AddressList.add(customer1address2);
+        customer1AddressList.add(address1Customer1);
+        customer1AddressList.add(address2Customer1);
 
-        Customer customer1 = new Customer(UUID.randomUUID(),"Kyle","kyle@gmail.com",customer1AddressList);
-        Customer customer2 = new Customer(UUID.randomUUID(),"Lilo","lilo@gmail.com");
-
+        Customer customer1 = new Customer(UUID.randomUUID(),"ozzy","ozzy@cydeo.com",customer1AddressList);
+        Customer customer2 = new Customer(UUID.randomUUID(),"mike","mike@gmail.com");
 
         StaticConstants.CUSTOMER_LIST.add(customer1);
         StaticConstants.CUSTOMER_LIST.add(customer2);
-
-
 
     }
 
     public static void createCategory(){
 
-        
+
+        Category category1 = new Electronic(UUID.randomUUID(),"Electronic");
+        Category category2 = new Furniture(UUID.randomUUID(),"Furniture");
+        Category category3 = new Skincare(UUID.randomUUID(),"Skincare");
+
+        StaticConstants.CATEGORY_LIST.add(category1);
+        StaticConstants.CATEGORY_LIST.add(category2);
+        StaticConstants.CATEGORY_LIST.add(category3);
+
 
     }
+
+
+    public static void createProduct(){
+
+        Product product1 =
+                new Product(UUID.randomUUID(),"PS5",230.72,7,5,StaticConstants.CATEGORY_LIST.get(0).getId());
+
+        Product product2 =
+                new Product(UUID.randomUUID(),"XBOX",120.34,15,5,StaticConstants.CATEGORY_LIST.get(0).getId());
+
+        Product product3 =
+                new Product(UUID.randomUUID(),"Chair",30.78,85,85,StaticConstants.CATEGORY_LIST.get(1).getId());
+
+        Product product4 =
+                new Product(UUID.randomUUID(),"Milk",3.78,5,2,UUID.randomUUID());
+
+
+
+
+        StaticConstants.PRODUCT_List.add(product1);
+        StaticConstants.PRODUCT_List.add(product2);
+        StaticConstants.PRODUCT_List.add(product3);
+        StaticConstants.PRODUCT_List.add(product4);
+
+
+    }
+
+
+    public static void createBalance() {
+
+        Balance customerBalance = new CustomerBalance(StaticConstants.CUSTOMER_LIST.get(0).getId(), 450.00);
+        Balance giftCardBalance = new GiftCardBalance(StaticConstants.CUSTOMER_LIST.get(0).getId(), 500.00);
+
+        StaticConstants.Customer_Balance_List.add(customerBalance);
+        StaticConstants.GiftCard_Balance_List.add(giftCardBalance);
+
+
+    }
+
+    public static void createDiscount(){
+
+
+        Discount amountBasedDiscount = new AmountBasedDiscount(UUID.randomUUID(),"Buy 250 Free 50",250.00,50.00);
+        Discount rateBasedDiscount = new RateBasedDiscount(UUID.randomUUID(),"Buy 500 Free 15%",500.00,15.00);
+
+        StaticConstants.DISCOUNT_LIST.add(amountBasedDiscount);
+        StaticConstants.DISCOUNT_LIST.add(rateBasedDiscount);
+
+
+
+
+    }
+
+
 
 
 
