fa8c47ddb5bb54e7cc2f8f795905b2d35707cff6
lgaid <104643045+lgaid@users.noreply.github.com>
2022-08-30 16:59:04 -0400

    Rate Based Discount updated

---
+++ src/discount/RateBasedDiscount.java
@@ -0,0 +1,27 @@
+package discount;
+
+import java.util.UUID;
+
+public class RateBasedDiscount extends Discount{
+
+    private Double rateAmount;
+
+    public RateBasedDiscount(UUID id, String name, Double thresholdAmount, Double rateAmount) {
+        super(id, name, thresholdAmount);
+        this.rateAmount = rateAmount;
+    }
+
+
+    @Override
+    public Double calculateCartAmountAfterDiscountApplied(Double amount) {
+        return amount - (amount * rateAmount / 100);
+    }
+
+
+    public Double getRateAmount(){
+        return rateAmount;
+    }
+
+
+
+}
